69f80abae6752c9c218b1cd2a561a555c699ce9f
JoshdfG <agboolajoshua161@gmail.com>
2024-01-29 04:04:51 +0100

    modified:   app/login/page.tsx modified:   app/signup/page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -19,7 +19,7 @@ export default function Signup() {
     <input type="password" id="password" className="shadow-sm bg-black border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required/>
   </div>
   <div className="mb-5">
-    <label htmlFor="repeat-password" className="block mb-2 text-sm font-medium  dark:text-white">Repeat password</label>
+    <label htmlFor="repeat-password" className="block mb-2 text-sm font-medium  dark:text-white">Confirm password</label>
     <input type="password" id="repeat-password" className="shadow-sm bg-black border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required/>
   </div>
   <div className="flex items-start mb-5">
