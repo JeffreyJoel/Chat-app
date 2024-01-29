32b804d7b615a826a22f434081eed9ced1df7e7d
JoshdfG <agboolajoshua161@gmail.com>
2024-01-29 03:24:01 +0100

    modified:   app/globals.css modified:   app/layout.tsx new file:   app/login/page.tsx modified:   app/page.tsx new file:   app/useFormEffect.tsx new file:   components.json new file:   components/ui/button.tsx new file:   components/ui/form.tsx new file:   components/ui/label.tsx new file:   lib/utils.ts deleted:    middleware.ts modified:   package-lock.json modified:   package.json modified:   tailwind.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,37 @@
+"use client"
 import React from "react";
+import Link from 'next/link'
+
 
 import "./globals.css"; // Make sure to import your global styles
 
 const YourComponent = () => {
+
+
   return (
     <div className="flex flex-col min-h-screen">
       <header>
-        <h1 className="font-extrabold text-2xl p-6">Z_<span className="text-[#0000ff]">Breed</span></h1>
+        <h1 className="font-extrabold text-2xl p-6">
+          Z_<span className="text-[#0000ff]">Breed</span>
+        </h1>
       </header>
       <main className="flex flex-col items-center flex-grow p-24">
-        <div className="mt-[25%]">
+        <div className="mt-[25%] md:mt-[10%]">
           <h1 className="capitalize font-bold text-xl">get started</h1>
         </div>
         <div className="mt-4">
-          <button className="capitalize text-md font-semibold  bg-[#3c46ff] p-3 rounded-lg mr-2 px-20 hover:bg-[#0000ff]">login</button>
-          <button className="capitalize text-md font-semibold  bg-[#3c46ff] p-3 rounded-lg mr-2 px-20  hover:bg-[#0000ff]">sign up</button>
+          <Link
+            href="/login"
+            className="capitalize text-md font-semibold bg-[#3c46ff] p-3 rounded-lg mr-2 px-20 hover:bg-[#0000ff]"
+            >
+            login
+          </Link>
+          <button className="capitalize text-md font-semibold bg-[#3c46ff] p-3 rounded-lg mr-2 px-20 hover:bg-[#0000ff]">
+            sign up
+          </button>
         </div>
       </main>
-      <footer className=" p-4">
+      <footer className="p-4">
         <h1 className="text-center">Z_Breed</h1>
       </footer>
     </div>
